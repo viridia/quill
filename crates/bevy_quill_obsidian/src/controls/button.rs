4b1c9da973e0e5fcd9a30a265a433d991fb3398c
Talin <viridia@gmail.com>
2024-08-08 15:42:39 -0700

    fix: Button min_height is also used for min_width.

--- crates/bevy_quill_obsidian/src/controls/button.rs
+++ crates/bevy_quill_obsidian/src/controls/button.rs
@@ -195,7 +195,9 @@ impl ViewTemplate for Button {
                 typography::text_default,
                 style_button,
                 move |ss: &mut StyleBuilder| {
-                    ss.min_height(size.height()).font_size(size.font_size());
+                    ss.min_height(size.height())
+                        .min_width(size.height())
+                        .font_size(size.font_size());
                     if minimal {
                         ss.padding(0);
                     } else {
